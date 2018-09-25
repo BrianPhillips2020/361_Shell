94d38043d211c4f58e0c0148647fd9aadcefe4ac
Brian Phillips <bphillip@ubuntu-1604.eecis.udel.edu>
2018-09-24 23:35:03 -0400

    Created user input loop in main and updated README

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BUFFERSIZE 124
 
-int main(){
-  printf("Marc $ux @ FTL\n");
+int main(int argc, char *argv[]){
+  char str[BUFFERSIZE];
+  char exit[4];
+  strcpy(exit, "exit");
+  while(strncmp(str, exit, 4) != 0){
+    printf(">");
+    fgets(str, BUFFERSIZE, stdin);
+   }
+  printf("Exiting Shell...\n");
   return 0;
 }
