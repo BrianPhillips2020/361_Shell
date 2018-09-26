7c3a9ba86abb0f3cfcea65235b59cf076e535745
Brian Phillips <bphillip@ubuntu-1604.eecis.udel.edu>
2018-09-25 22:37:41 -0400

    Copied over the skeleton

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
+#include "sh.h"
+#include <signal.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define BUFFERSIZE 124
 
-int main(int argc, char *argv[]){
-  char str[BUFFERSIZE];
-  char exit[4];
-  strcpy(exit, "exit");
-  while(strncmp(str, exit, 4) != 0){
-    printf(">");
-    fgets(str, BUFFERSIZE, stdin);
-   }
-  printf("Exiting Shell...\n");
-  return 0;
+void sig_handler(int signal);
+
+int main( int argc, char **argv, char **envp )
+{
+  /* put signal set up stuff here */
+
+  return sh(argc, argv, envp);
+}
+
+void sig_handler(int signal)
+{
+  /* define your signal handler */
 }
